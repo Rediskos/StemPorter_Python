ff06ac1b6ee707035b084e696e857f879d6b33d5
Rediskos <danil0662015046@gmail.com>
2020-04-19 18:12:15 +0300

    Update ClasificatorKNN.py

--- ClasificatorKNN.py
+++ ClasificatorKNN.py
@@ -1,156 +1,190 @@
-import requests
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-import os
-import numpy as np
-import math
 from tqdm import tqdm
-import corr
-import StemPorter 
 
-def corr_text(text):
-    ans = ""
+Vowels = "аеиоуыеюя"
+
+def procesString(string):
+    tmp = string
+    tmp.replace("  ", " ")
+    tmp = tmp.split()
     
-    for i in text.splitlines():
-        for j in i.split():
-            j = StemPorter.Stem(j)
-            j = corr.checkWord(j)
-            j = j.lower()
-            ans += j.strip()
-            if(len(j) > 0 and j != " "):
-                ans += " "
-    return ans
+    return [tmp[i] for i in range(0, len(tmp), 2)]
 
-Base = {}
-AllPoints = []
+PERFECTIVE_GERUND = [["в", "вши", "вшись"],
+                     ["ив", "ивши", "ившись", "ыв", "ывши", "ывшись"]]
+ADJECTIVE = procesString("ее (ee)   ие (ie)   ые (ye)   ое (oe)   ими (imi)   ыми (ymi)   ей (eì)   ий (iì)   ый (yì)   ой (oì)   ем (em)   им (im)   ым (ym)   ом (om)   его (ego)   ого (ogo)   ему (emu)   ому (omu)   их (ikh)   ых (ykh)   ую (uiu)   юю (iuiu)   ая (aia)   яя (iaia)   ою (oiu)   ею (eiu)")
+PARTICIPLE = [ procesString("ем (em)   нн (nn)   вш (vsh)   ющ (iushch)   щ (shch)") ,
+               procesString("ивш (ivsh)   ывш (yvsh)   ующ (uiushch)") ]
+REFLEXIVE =procesString("ся (sia)   сь (s')")
+VERB = [ procesString("ла (la)   на (na)   ете (ete)   йте (ìte)   ли (li)   й (ì)   л (l)   ем (em)   н (n)   ло (lo)   но (no)   ет (et)   ют (iut)   ны (ny)   ть (t')   ешь (esh')   нно (nno)") ,
+        procesString("ила (ila)   ыла (yla)   ена (ena)   ейте (eìte)   уйте (uìte)   ите (ite)   или (ili)   ыли (yli)   ей (eì)   уй (uì)   ил (il)   ыл (yl)   им (im)   ым (ym)   ен (en)   ило (ilo)   ыло (ylo)   ено (eno)   ят (iat)   ует (uet)   уют (uiut)   ит (it)   ыт (yt)   ены (eny)   ить (it')   ыть (yt')   ишь (ish')   ую (uiu)   ю (iu)") ]
+NOUN = procesString("а (a)   ев (ev)   ов (ov)   ие (ie)   ье ('e)   е (e)   иями (iiami)   ями (iami)   ами (ami)   еи (ei)   ии (ii)   и (i)   ией (ieì)   ей (eì)   ой (oì)   ий (iì)   й (ì)   иям (iiam)   ям (iam)   ием (iem)   ем (em)   ам (am)   ом (om)   о (o)   у (u)   ах (akh)   иях (iiakh)   ях (iakh)   ы (y)   ь (')   ию (iiu)   ью ('iu)   ю (iu)   ия (iia)   ья ('ia)   я (ia)")
+SUPERLATIVE = procesString("ейш (eìsh)   ейше (eìshe)")
+DERIVATIONAL = procesString("ост (ost)   ость (ost')")
 
-# dict [x,y,z]:class
-def loadBase():
-    with open("BD_Med_Sport_Math/Sport/SportPoints.txt", "r", encoding = "utf-8") as inpt:
-        for i in inpt.readlines():
-            tmp = i.split()
-            Base[i.strip()] = "Sport"
-            AllPoints.append(tmp)
-            
-    with open("BD_Med_Sport_Math/Med/MedPoints.txt", "r", encoding = "utf-8") as inpt:
-        for i in inpt.readlines():
-            tmp = i.split()
-            Base[i.strip()] = "Med"
-            AllPoints.append(tmp)
-            
-    with open("BD_Med_Sport_Math/Math/MathPoints.txt", "r", encoding = "utf-8") as inpt:
-        for i in inpt.readlines():
-            tmp = i.split()
-            Base[i.strip()] = "Math"
-            AllPoints.append(tmp)
-            
-
-def get_text(url):
-    rs = requests.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
+def findeRV_R1_R2(word):
+    ptr = 0
+    ans = {"RV": len(word), "R1": len(word) + 1, "R2": len(word) + 1}
     
-    return corr_text(root.text).split()
-            
-def calcScore(url, text = False):
-    if(text == False):
-        tmp = get_text(url)
-    else:
-        tmp = url
+    for i in range(ptr, len(word)):
+        if word[i] in Vowels:
+            ptr += 1
+            ans["RV"] = ptr
+            break
+        ptr += 1
     
-    tmp = [i.strip() for i in tmp]
+    for i in range(ptr, len(word)):
+        if word[i] not in Vowels:
+            ptr += 1
+            ans["R1"] = ptr
+            break
+        ptr += 1
     
-    SportScore = 0
-    BioScore = 0
-    MathScore = 0
-    cnt = 0
-    
-    for i in SportTerms:
-        CountWords = tmp.count(i)
-        if CountWords != 0:
-            SportScore += CountWords
-    
-    for i in BiosTerms:
-        CountWords = tmp.count(i)
-        if CountWords != 0:
-            BioScore += CountWords
-    
-    for i in MathTerms:
-        CountWords = tmp.count(i)
-        if CountWords != 0:
-            MathScore += CountWords
-    
-    
-    All = SportScore + BioScore + MathScore
-    
-    return [SportScore / All * 100, BioScore / All * 100, MathScore / All * 100]
-    
-def norma(a, b):
-    ans = 0
-    for i in range(3):
-        ans += float(a[i]) * float(b[i]) * float(a[i]) * float(b[i])
-    return ans
-
-
-SportTerms = []
-with open("BD/Sport/SportTerms.txt", "r", encoding = "utf-8") as ST:
-    for i in ST.readlines():
-        for j in i.split():
-            SportTerms.append(j)
-
-MathTerms = []
-with open("BD/Math/RealMathTerms.txt", "r", encoding = "utf-8") as ST:
-    for i in ST.readlines():
-        for j in i.split():
-            MathTerms.append(j)
-            
-BiosTerms = []
-with open("BD/Biologic/RealBio.txt", "r", encoding = "utf-8") as ST:
-    for i in ST.readlines():
-        for j in i.split():
-            BiosTerms.append(j)
-
-print("OUT")
-
-loadBase()
-
-SPORT = "BD_Med_Sport_Math/Sport/"
-MEDIC = "BD_Med_Sport_Math/Med/"
-MATH = "BD_Med_Sport_Math/Math/"
-LABELS = {SPORT:0, MEDIC: 1, MATH: 0}
-FORLOSS = {0: "Sport", 1:"Med", 2:"Math"}
-
-loss = 0
-
-for label in LABELS:
-    for f in tqdm(os.listdir(label)):
+    for i in range(ptr, len(word)):
+        if word[i] in Vowels:
+            ptr += 1
+            break
+        ptr += 1
         
-        if("Point" in f):
-            pass
-        else:
-            path = os.path.join(label, f)
-            for k in range(len(AllPoints), 0, -1)
-            with open(path, "r", encoding = "utf-8") as inpt:
-                for url in tqdm(inpt.readlines()):
-                    scors = {"Sport":0, "Med":0, "Math":0}
-                                        
-                    url = url.strip()
-                    target = calcScore(url, text = False)
-                    
-                    
-                    tmp = sorted(AllPoints, key = lambda x: norma(x, target))
-                    
-                    #print(tmp)
-                    
-                    cnt = 2
-                    maxv = 0
-                    maxName = ""
-                    
-                    for i in tmp:
-                        word = str(i[0]) + " " + str(i[1]) + " " + str(i[2])
-                        scors[Base[word]] += 100 / math.log(norma(i, target))
-                        if(maxv < scors[Base[word]]):
-                            maxv = scors[Base[word]]
-                            maxName = Base[word]
-                    if(maxName != FORLOSS[LABELS[label]]):
-                        loss+= 1
-print("Loss is ", loss)
+    for i in range(ptr, len(word)):
+        if word[i] not in Vowels:
+            ptr += 1
+            ans["R2"] = ptr
+            break
+        ptr += 1
+        
+    
+    return ans
+
+        
+def StemSTEP1(word, RV_R1_R2):
+    maxW = ""
+    
+    for i in PERFECTIVE_GERUND[0]:
+        if(word.find("а" + i, RV_R1_R2["RV"]) != -1 or
+           word.find("я" + i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+                
+    for i in PERFECTIVE_GERUND[1]:
+        if(word.find(i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+                
+    if(maxW != ""):
+        return word[:-len(maxW)]
+    
+    maxW = ""
+    for i in REFLEXIVE:
+        if(word.find(i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+    
+    if(maxW != ""):
+        word = word[:-len(maxW)]
+    
+    maxW = ""
+    for i in ADJECTIVE:
+        if(word.find(i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+    
+    for i in PARTICIPLE[0]:
+        for j in ADJECTIVE:
+            if(word.find("а" + i + j, RV_R1_R2["RV"]) != -1 or
+               word.find("я" + i + j, RV_R1_R2["RV"]) != -1):
+                if(len(i + j) > len(maxW)):
+                    maxW = i + j
+                
+    for i in PARTICIPLE[1]:
+        for j in ADJECTIVE:
+            if(word.find(i + j, RV_R1_R2["RV"]) != -1):
+                if(len(i + j) > len(maxW)):
+                    maxW = i + j
+    
+    if(maxW != ""):
+        return word[:-len(maxW)]
+    
+    maxW = ""
+    
+    for i in VERB[0]:
+        if(word.find("а" + i, RV_R1_R2["RV"]) != -1 or
+           word.find("я" + i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+                
+    for i in VERB[1]:
+        if(word.find(i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+                
+    if(maxW != ""):
+        return word[:-len(maxW)]
+    
+    maxW = ""
+    for i in NOUN:
+        if(word.find(i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+    
+    if(maxW != ""):
+        return word[:-len(maxW)]
+    
+    return word
+
+
+def StemSTEP2(word):
+    if(word[-1] == 'и'):
+        return word[:-1]
+    return word
+    
+
+
+def StemSTEP3(word, RV_R1_R2):
+    
+    maxW = ""
+    for i in DERIVATIONAL:
+        if(word.find(i, RV_R1_R2["RV"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+    
+    if(maxW != ""):
+        return word[:-len(maxW)]
+    return word
+    
+
+def StemSTEP4(word, RV_R1_R2):
+    
+    maxW = ""
+    for i in SUPERLATIVE:
+        if(word.find(i, RV_R1_R2["R2"]) != -1):
+            if(len(i) > len(maxW)):
+                maxW = i
+    
+    if(maxW != ""):
+        word = word[:-len(maxW)]
+    
+    if(word.find("нн", RV_R1_R2["RV"]) != -1):
+        i = word.find("нн", RV_R1_R2["RV"])
+        word = word[:i]
+    
+    if(word[-1] == "ь"):
+        word = word[:-1]
+    return word
+
+
+def Stem(word):
+    
+    if(len(word) < 2):
+        return word
+    
+    RV_R1_R2 = findeRV_R1_R2(word)
+    
+    word = StemSTEP1(word, RV_R1_R2)
+    word = StemSTEP2(word)
+    word = StemSTEP3(word, RV_R1_R2)
+    word = StemSTEP4(word, RV_R1_R2)
+    return word
+    
+    
+    
+    
